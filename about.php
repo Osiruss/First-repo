bf6a08ba13ff9251e9f5166d8dba66ff79b340c6
Russell Kitchen <russellkitchen@live.com>
2015-10-13 10:06:24 +0100

    Add content to about.php including formatting.

---
+++ about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About me</title>
+</head>
+<body>
+	<h1>Lorem ipsum dolor sit amet.</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore cum minus necessitatibus fugiat maiores voluptatem consectetur corporis natus voluptate sequi ea voluptates cumque animi nulla magni sunt incidunt, ipsam quos qui sint, illo beatae delectus.</p>
+</body>
+</html>
