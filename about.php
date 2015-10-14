00eaa58ee9981423747cd7a524f73e270e0dc1de
Russell Kitchen <russellkitchen@live.com>
2015-10-14 14:02:48 +0100

    Added a third paragraph lorem filled tag

--- about.php
+++ about.php
@@ -8,5 +8,6 @@
 	<h1>Lorem ipsum dolor sit amet.</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore cum minus necessitatibus fugiat maiores voluptatem consectetur corporis natus voluptate sequi ea voluptates cumque animi nulla magni sunt incidunt, ipsam quos qui sint, illo beatae delectus.</p>
 	<p>Cras ultricies mi eu</p>
+	<p>Lorem ipsum dolor sit amet, consectetur.</p>
 </body>
 </html>
